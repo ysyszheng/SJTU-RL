42f2af5221117976a45c4f6baf2128635deb4ec7
Yusen Zheng <ysyszheng@gmail.com>
2023-04-08 21:10:46 +0800

    update

---
+++ A4/main.py
@@ -0,0 +1,108 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+import gymnasium as gym
+import random
+from collections import deque
+import matplotlib.pyplot as plt
+import time
+
+# Hyperparameters
+EPISODES = 1000
+BATCH_SIZE = 32
+GAMMA = 0.99
+EPSILON = 1.0
+EPSILON_DECAY = 0.999
+EPSILON_MIN = 0.01
+LEARNING_RATE = 0.001
+TARGET_UPDATE = 10
+MEMORY_SIZE = 10000
+
+# DQN Agent
+class DQNAgent:
+    def __init__(self, state_size, action_size):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.memory = deque(maxlen=MEMORY_SIZE)
+        self.gamma = GAMMA
+        self.epsilon = EPSILON
+        self.epsilon_min = EPSILON_MIN
+        self.epsilon_decay = EPSILON_DECAY
+        self.learning_rate = LEARNING_RATE
+        self.model = self.build_model()
+        self.target_model = self.build_model()
+        self.update_target_model()
+
+    def build_model(self):
+        model = nn.Sequential(
+            nn.Linear(self.state_size, 24),
+            nn.ReLU(),
+            nn.Linear(24, 24),
+            nn.ReLU(),
+            nn.Linear(24, self.action_size),
+        )
+        model.compile(loss='mse', optimizer=optim.Adam(self.learning_rate))
+        return model
+
+    def update_target_model(self):
+        self.target_model.set_weights(self.model.get_weights())
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def act(self, state):
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_size)
+        act_values = self.model.predict(state)
+        return np.argmax(act_values[0])
+
+    def replay(self, batch_size):
+        minibatch = random.sample(self.memory, batch_size)
+        for state, action, reward, next_state, done in minibatch:
+            target = reward
+            if not done:
+                target = (reward + self.gamma *
+                          np.amax(self.target_model.predict(next_state)[0]))
+            target_f = self.model.predict(state)
+            target_f[0][action] = target
+            self.model.fit(state, target_f, epochs=1, verbose=0)
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+    def load(self, name):
+        self.model.load_weights(name)
+
+    def save(self, name):
+        self.model.save_weights(name)
+
+if __name__ == "__main__":
+    env = gym.make('MountainCar-v0', render_mode='human')
+    state_size = env.observation_space.shape[0]
+    action_size = env.action_space.n
+    agent = DQNAgent(state_size, action_size)
+    done = False
+    batch_size = BATCH_SIZE
+
+    for e in range(EPISODES):
+        state = env.reset()
+        state = np.reshape(state, [1, state_size])
+        for time in range(500):
+            env.render()
+            action = agent.act(state)
+            next_state, reward, done, _ = env.step(action)
+            reward = reward if not done else -10
+            next_state = np.reshape(next_state, [1, state_size])
+            agent.remember(state, action, reward, next_state, done)
+            state = next_state
+            if done:
+                print("episode: {}/{}, score: {}, e: {:.2}"
+                      .format(e, EPISODES, time, agent.epsilon))
+                break
+            if len(agent.memory) > batch_size:
+                agent.replay(batch_size)
+        if e % TARGET_UPDATE == 0:
+            agent.update_target_model()
+
+    env.close()
