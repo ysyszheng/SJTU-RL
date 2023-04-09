1f65119af06576e18b2f386ebb5c05ef11c41089
Yusen Zheng <ysyszheng@gmail.com>
2023-04-09 12:22:40 +0800

    update

--- A4/main.py
+++ A4/main.py
@@ -34,6 +34,7 @@ class DQNAgent:
         self.model = self.build_model()
         self.target_model = self.build_model()
         self.update_target_model()
+        self.loss = []
 
     def build_model(self):
         model = nn.Sequential(
@@ -43,11 +44,10 @@ class DQNAgent:
             nn.ReLU(),
             nn.Linear(24, self.action_size),
         )
-        model.compile(loss='mse', optimizer=optim.Adam(self.learning_rate))
         return model
 
     def update_target_model(self):
-        self.target_model.set_weights(self.model.get_weights())
+        self.target_model.load_state_dict(self.model.state_dict())
 
     def remember(self, state, action, reward, next_state, done):
         self.memory.append((state, action, reward, next_state, done))
@@ -55,19 +55,32 @@ class DQNAgent:
     def act(self, state):
         if np.random.rand() <= self.epsilon:
             return random.randrange(self.action_size)
-        act_values = self.model.predict(state)
-        return np.argmax(act_values[0])
+        with torch.no_grad():
+            state = torch.from_numpy(state).float()
+            act_values = self.model(state)
+            return np.argmax(act_values[0]).detach().numpy()
 
     def replay(self, batch_size):
         minibatch = random.sample(self.memory, batch_size)
-        for state, action, reward, next_state, done in minibatch:
-            target = reward
-            if not done:
-                target = (reward + self.gamma *
-                          np.amax(self.target_model.predict(next_state)[0]))
-            target_f = self.model.predict(state)
-            target_f[0][action] = target
-            self.model.fit(state, target_f, epochs=1, verbose=0)
+        state = torch.from_numpy(np.vstack([i[0] for i in minibatch])).float()
+        action = torch.from_numpy(np.vstack([i[1] for i in minibatch])).long()
+        reward = torch.from_numpy(np.vstack([i[2] for i in minibatch])).float()
+        next_state = torch.from_numpy(np.vstack([i[3] for i in minibatch])).float()
+        done = torch.from_numpy(np.vstack([i[4] for i in minibatch]).astype(np.uint8)).float()
+
+        Q_value = self.model(state).gather(1, action)
+        Q_next = self.target_model(next_state).detach().max(1)[0].unsqueeze(1)
+        Q_target = reward + (self.gamma * Q_next * (1 - done))
+        loss = F.mse_loss(Q_value, Q_target)
+        optimizer = optim.Adam(self.model.parameters(), lr=self.learning_rate)
+        # self.loss.append(loss.item())
+
+        self.model.zero_grad()
+        loss.backward()
+        for param in self.model.parameters():
+            param.grad.data.clamp_(-1, 1)
+        optimizer.step()
+
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
 
@@ -78,7 +91,7 @@ class DQNAgent:
         self.model.save_weights(name)
 
 if __name__ == "__main__":
-    env = gym.make('MountainCar-v0', render_mode='human')
+    env = gym.make('MountainCar-v0', render_mode='human').env # avoid truncation
     state_size = env.observation_space.shape[0]
     action_size = env.action_space.n
     agent = DQNAgent(state_size, action_size)
@@ -86,12 +99,12 @@ if __name__ == "__main__":
     batch_size = BATCH_SIZE
 
     for e in range(EPISODES):
-        state = env.reset()
+        state, info = env.reset()
         state = np.reshape(state, [1, state_size])
         for time in range(500):
             env.render()
             action = agent.act(state)
-            next_state, reward, done, _ = env.step(action)
+            next_state, reward, done, truncated, info = env.step(action)
             reward = reward if not done else -10
             next_state = np.reshape(next_state, [1, state_size])
             agent.remember(state, action, reward, next_state, done)
@@ -99,10 +112,15 @@ if __name__ == "__main__":
             if done:
                 print("episode: {}/{}, score: {}, e: {:.2}"
                       .format(e, EPISODES, time, agent.epsilon))
+                agent.loss.append(time)
                 break
             if len(agent.memory) > batch_size:
                 agent.replay(batch_size)
         if e % TARGET_UPDATE == 0:
             agent.update_target_model()
 
-    env.close()
+    # agent.save("./save/mountaincar-dqn.h5")
+    plt.plot([i+1 for i in range(0, len(agent.loss), 2)], agent.loss[::2])
+    plt.ylabel('Episode Length')
+    plt.xlabel('Episode')
+    plt.show()
