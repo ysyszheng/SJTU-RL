c1d09a37b6d7c1c1c020ba24e11dbcaa34414fef
Yusen Zheng <ysyszheng@gmail.com>
2023-05-04 18:03:22 +0800

    update

--- A5/models/a3c.py
+++ A5/models/a3c.py
@@ -5,4 +5,64 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class Actor(nn.Module):
-  pass
+  def __init__(self, state_dim, action_dim, max_action):
+    super(Actor, self).__init__()
+    self.max_action = max_action
+    self.net = nn.Sequential(
+      nn.Linear(state_dim, 256),
+      nn.ReLU(),
+      nn.Linear(256, 128),
+      nn.ReLU(),
+      nn.Linear(128, action_dim),
+      nn.Tanh()
+    )
+
+  def forward(self, state):
+    return self.net(state) * self.max_action
+
+  def act(self, state):
+    with torch.no_grad():
+      action = self.forward(state).cpu().data.numpy().flatten()
+    return action
+  
+  def act_with_noise(self, state, noise):
+    with torch.no_grad():
+      action = self.forward(state).cpu().data.numpy().flatten()
+    return (action + noise).clip(-self.max_action, self.max_action)
+  
+class Critic(nn.Module):
+  def __init__(self, state_dim, action_dim):
+    super(Critic, self).__init__()
+    self.net = nn.Sequential(
+      nn.Linear(state_dim + action_dim, 256),
+      nn.ReLU(),
+      nn.Linear(256, 128),
+      nn.ReLU(),
+      nn.Linear(128, 1)
+    )
+
+  def forward(self, state, action):
+    return self.net(torch.cat([state, action], 1))
+  
+class A3CWorker(object):
+  def __init__(self, state_dim, action_dim, max_action, worker_id, 
+               global_actor, global_critic, optimizer, device=torch.device("cpu")):
+    self.worker_id = worker_id
+    self.actor = Actor(state_dim, action_dim, max_action).to(device)
+    self.critic = Critic(state_dim, action_dim).to(device)
+    self.global_actor = global_actor
+    self.global_critic = global_critic
+    self.optimizer = optimizer
+    self.device = device
+
+    # for actor_param in self.
+
+  # def update(self, replay_buffer, batch_size, discount, tau, policy_noise, noise_clip, policy_freq):
+
+class A3C(object):
+  def __init__(self, num_works, state_dim, action_dim, max_action, lr, device=torch.device("cpu")):
+    self.num_works = num_works
+    self.workers = [A3CWorker() for _ in range(num_works)]
+    self.global_actor = Actor(state_dim, action_dim, max_action).to(device)
+    self.global_critic = Critic(state_dim, action_dim).to(device)
+    self.optimizer = torch.optim.Adam(self.actor.parameters(), lr)
--- README.md
+++ README.md
@@ -1,8 +1,28 @@
-Mountain Car
+Env
+-----
+```bash
+pip install gymnasium
+pip install "gymnasium[classic-control]"
+```
+
+A4 - Mountain Car
 -----
 [Mountain Car](https://gymnasium.farama.org/environments/classic_control/mountain_car/)
 
 ```bash
-pip install gymnasium
-pip install "gymnasium[classic-control]"
+# Train
+python3 ./train.py [Model Name: DQN/DoubleDQN/DuelingDQN]
+# Test
+python3 ./test.py [Model Name: DQN/DoubleDQN/DuelingDQN]
+```
+
+A5 - Pendulum
+-----
+[Pendulum](https://gymnasium.farama.org/environments/classic_control/pendulum/)
+
+```bash
+# Train
+python3 ./run.py --mode train --model [Model Name: A3C or DDPG] --config [Config Path: ./config/a3c.yaml or ./config/ddpg.yaml]
+# Test
+python3 ./run.py --mode test --model [Model Name: A3C or DDPG] --config [Config Path: ./config/a3c.yaml or ./config/ddpg.yaml]
 ```
