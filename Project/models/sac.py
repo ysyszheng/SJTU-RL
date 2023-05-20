1643b27dca9f1195026364ea342e44a25a5fbfbe
Yusen Zheng <ysyszheng@gmail.com>
2023-05-20 23:04:01 +0800

    update

--- Project/models/sac.py
+++ Project/models/sac.py
@@ -4,5 +4,131 @@ import torch.nn.functional as F
 import numpy as np
 import random
 
-class Net(nn.Module):
-    pass
+# Soft Actor-Critic (SAC)
+class Actor(nn.Module):
+    def __init__(self, state_dim, action_dim, max_action):
+        super(Actor, self).__init__()
+
+        self.l1 = nn.Linear(state_dim, 256)
+        self.l2 = nn.Linear(256, 256)
+        self.l3 = nn.Linear(256, action_dim)
+
+        self.max_action = max_action
+
+    def forward(self, state):
+        a = F.relu(self.l1(state))
+        a = F.relu(self.l2(a))
+        return self.max_action * torch.tanh(self.l3(a))
+    
+class Critic(nn.Module):
+    def __init__(self, state_dim, action_dim):
+        super(Critic, self).__init__()
+
+        # Q1 architecture
+        self.l1 = nn.Linear(state_dim + action_dim, 256)
+        self.l2 = nn.Linear(256, 256)
+        self.l3 = nn.Linear(256, 1)
+
+        # Q2 architecture
+        self.l4 = nn.Linear(state_dim + action_dim, 256)
+        self.l5 = nn.Linear(256, 256)
+        self.l6 = nn.Linear(256, 1)
+
+    def forward(self, state, action):
+        sa = torch.cat([state, action], 1)
+
+        q1 = F.relu(self.l1(sa))
+        q1 = F.relu(self.l2(q1))
+        q1 = self.l3(q1)
+
+        q2 = F.relu(self.l4(sa))
+        q2 = F.relu(self.l5(q2))
+        q2 = self.l6(q2)
+        return q1, q2
+    
+    def Q1(self, state, action):
+        sa = torch.cat([state, action], 1)
+
+        q1 = F.relu(self.l1(sa))
+        q1 = F.relu(self.l2(q1))
+        q1 = self.l3(q1)
+        return q1
+    
+class SAC(object):
+    def __init__(self, state_dim, action_dim, max_action, device=torch.device('cpu')):
+        self.device = device
+        self.actor = Actor(state_dim, action_dim, max_action).to(device)
+        self.actor_target = Actor(state_dim, action_dim, max_action).to(device)
+        self.actor_target.load_state_dict(self.actor.state_dict())
+        self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=3e-4)
+
+        self.critic = Critic(state_dim, action_dim).to(device)
+        self.critic_target = Critic(state_dim, action_dim).to(device)
+        self.critic_target.load_state_dict(self.critic.state_dict())
+        self.critic_optimizer = torch.optim.Adam(self.critic.parameters(), lr=3e-4)
+
+        self.max_action = max_action
+
+    def select_action(self, state):
+        state = torch.FloatTensor(state.reshape(1, -1)).to(self.device)
+        return self.actor(state).cpu().data.numpy().flatten()
+    
+    def sample_action(self, state):
+        state = torch.FloatTensor(state.reshape(1, -1)).to(self.device)
+        return self.actor(state).cpu().data.numpy().flatten() + np.random.normal(0, self.max_action/10, size=self.max_action.shape)
+    
+    def train(self, replay_buffer, iterations, batch_size=256, discount=0.99, tau=0.005, policy_noise=0.2, noise_clip=0.5, policy_freq=2):
+        for it in range(iterations):
+            # Sample replay buffer 
+            s, a, s_, r, d = replay_buffer.sample(batch_size)
+            state = torch.FloatTensor(s).to(self.device)
+            action = torch.FloatTensor(a).to(self.device)
+            next_state = torch.FloatTensor(s_).to(self.device)
+            done = torch.FloatTensor(1 - d).to(self.device)
+            reward = torch.FloatTensor(r).to(self.device)
+
+            # Select action according to policy and add clipped noise
+            noise = torch.FloatTensor(a).data.normal_(0, policy_noise).to(self.device)
+            noise = noise.clamp(-noise_clip, noise_clip)
+            next_action = (self.actor_target(next_state) + noise).clamp(-self.max_action, self.max_action)
+
+            # Compute the target Q value
+            target_Q1, target_Q2 = self.critic_target(next_state, next_action)
+            target_Q = torch.min(target_Q1, target_Q2)
+            target_Q = reward + (done * discount * target_Q).detach()
+
+            # Get current Q estimates
+            current_Q1, current_Q2 = self.critic(state, action)
+
+            # Compute critic loss
+            critic_loss = F.mse_loss(current_Q1, target_Q) + F.mse_loss(current_Q2, target_Q)
+
+            # Optimize the critic
+            self.critic_optimizer.zero_grad()
+            critic_loss.backward()
+            self.critic_optimizer.step()
+            
+            # Delayed policy updates
+            if it % policy_freq == 0:
+                # Compute actor loss
+                actor_loss = -self.critic.Q1(state, self.actor(state)).mean()
+                
+                # Optimize the actor 
+                self.actor_optimizer.zero_grad()
+                actor_loss.backward()
+                self.actor_optimizer.step()
+
+                # Update the frozen target models
+                for param, target_param in zip(self.critic.parameters(), self.critic_target.parameters()):
+                    target_param.data.copy_(tau * param.data + (1 - tau) * target_param.data)
+
+                for param, target_param in zip(self.actor.parameters(), self.actor_target.parameters()):
+                    target_param.data.copy_(tau * param.data + (1 - tau) * target_param.data)
+
+    def save(self, filename):
+        torch.save(self.actor.state_dict(), filename + "_actor")
+        torch.save(self.critic.state_dict(), filename + "_critic")
+
+    def load(self, filename):
+        self.actor.load_state_dict(torch.load(filename + "_actor"))
+        self.critic.load_state_dict(torch.load(filename + "_critic"))
--- Project/scripts/sac/train.py
+++ Project/scripts/sac/train.py
@@ -1,2 +1,70 @@
+import gym
+import torch
+import numpy as np
+from tqdm import tqdm
+from utils.fix_seed import fix_seed
+from utils.replay_buffer import ReplayBuffer
+from models.sac import SAC
+
+
 class Trainer(object):
-  pass
+    def __init__(self, config):
+        # create env
+        self.config = config
+        # self.env = gym.make(config['env'], max_episode_steps=self.config['max_steps'])
+        self.env = gym.make(config['env'])
+        fix_seed(self.config['seed'])
+
+        # params
+        state_dim = self.env.observation_space.shape[0]
+        action_dim = self.env.action_space.shape[0]
+        max_action = float(self.env.action_space.high[0])
+        device = torch.device('cpu')
+        lr = self.config['lr']
+        gamma = self.config['gamma']
+        tau = self.config['tau']
+        batch_size = self.config['batch_size']
+        memory_size = self.config['memory_size']
+
+        # model
+        self.agent = SAC(state_dim, action_dim, max_action,
+                          lr, gamma, tau, batch_size, device)
+        self.replay_buffer = ReplayBuffer(memory_size)
+
+    def train(self):
+        total_step = 0
+        bar = tqdm(range(self.config['num_episodes']))
+        r = []
+
+        for episode in bar:
+            state, _ = self.env.reset()
+            # state, _ = self.env.reset(seed = self.config['seed'] + episode)
+            episode_reward = 0
+
+            while True:
+                total_step += 1
+                noise = np.random.normal(
+                    0, self.config['noise_std'], size=self.env.action_space.shape[0])
+                action = self.agent.select_action_with_noise(state, noise)
+
+                next_state, reward, terminated, truncated, _ = self.env.step(
+                    action)
+                self.replay_buffer.add(
+                    (state, action, next_state, reward, terminated))
+                state = next_state
+                episode_reward += reward if not truncated else 0 # ignore truncated
+
+                if total_step > self.config['warmup_steps']:
+                    self.agent.update(self.replay_buffer,
+                                      self.config['num_epochs'])
+                if terminated or truncated:
+                    break
+
+            bar.set_description('Episode: {}/{} | Episode Reward: {:.2f} | Truncated: {} | Terminated: {}'.
+                                format(episode+1, self.config['num_episodes'], episode_reward, truncated, terminated))
+            r.append(episode_reward)
+
+        # save rewards
+        np.save("./out/datas/" + self.config['env'] + "/ddpg.npy", r)
+
+        self.agent.save("./out/models/" + self.config['env'] + "/ddpg")
