314779ef281f38416070d768181ab4b6ff0df6ce
Yusen Zheng <ysyszheng@gmail.com>
2023-05-23 08:38:45 +0800

    update

--- Project/out/images/PongNoFrameskip-v4/plot.py
+++ Project/out/images/PongNoFrameskip-v4/plot.py
@@ -3,10 +3,10 @@ import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     plt.figure(dpi=300)
-    # rewards_dqn = np.load('./out/datas/PongNoFrameskip-v4/dqn.npy')
+    rewards_dqn = np.load('./out/datas/PongNoFrameskip-v4/dqn.npy')
     rewards_ddqn = np.load('./out/datas/PongNoFrameskip-v4/ddqn.npy')
     rewards_d3qn = np.load('./out/datas/PongNoFrameskip-v4/d3qn.npy')
-    # plt.plot(rewards_dqn, label='DQN')
+    plt.plot(rewards_dqn, label='DQN')
     plt.plot(rewards_ddqn, label='DDQN')
     plt.plot(rewards_d3qn, label='D3QN')
     plt.xlabel('Episode')
